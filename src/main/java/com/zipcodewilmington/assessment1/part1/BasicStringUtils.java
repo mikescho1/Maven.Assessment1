a7d4ddfc98546652265c756796fe734ad3b08387
mike <mike.schoenbeck@gmail.com>
2019-10-28 10:29:03 -0400

    string utils complete except for inverse case

--- src/main/java/com/zipcodewilmington/assessment1/part1/BasicStringUtils.java
+++ src/main/java/com/zipcodewilmington/assessment1/part1/BasicStringUtils.java
@@ -11,7 +11,7 @@ public class BasicStringUtils {
     public static String camelCase(String str) {
         String str2 = str.substring(0, 1).toUpperCase() + str.substring(1);
 
-        return  str2;
+        return str2;
     }
 
     /**
@@ -20,7 +20,7 @@ public class BasicStringUtils {
      */
     public static String reverse(String str) {
         String str2 = "";
-        for(Integer i = str.length() - 1; i >= 0; i--)  {
+        for (Integer i = str.length() - 1; i >= 0; i--) {
             str2 += str.charAt(i);
         }
 
@@ -34,9 +34,8 @@ public class BasicStringUtils {
     public static String reverseThenCamelCase(String str) {
         String str2 = reverse(str);
         String str3 = camelCase(str2);
-            return str3;
-        }
-
+        return str3;
+    }
 
 
     /**
@@ -44,7 +43,7 @@ public class BasicStringUtils {
      * @return string with identical contents excluding first and last character
      */
     public static String removeFirstAndLastCharacter(String str) {
-        String str2 = str.substring(1, str.length()-1);
+        String str2 = str.substring(1, str.length() - 1);
 
         return str2;
 
@@ -56,6 +55,24 @@ public class BasicStringUtils {
      * @return string with identical characters, each with opposite casing
      */
     public static String invertCasing(String str) {
+
         return null;
+
+        /*String[] strArr = str.split("");
+        for (Integer i = 0; i < strArr.length; i++) {
+                if (strArr[i] == Character.isUpperCase(strArr[i])) {
+                    char letter = Character.toLowerCase(strArr[i].charAt(j));
+                    str2 += letter;
+                }
+                else if (Character.isLowerCase(strArr[i].charAt(j))) {
+                    char letter = Character.toLowerCase(strArr[i].charAt(j));
+                    str2 += letter;
+                } else  {
+                    str2 += " ";
+
+                }
+            }
+        }
+        return str2;*/
     }
 }
